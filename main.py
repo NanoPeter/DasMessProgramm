576d26edc7eb5f5a73d0859e25e56e9a6b214626
t2uni <s2360026@stud.uni-frankfurt.de>
2018-05-15 13:43:45 +0200

    Add input widgets dynamically depending on measurement type.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtWidgets import QApplication
 from threading import Thread
 
+from measurement.measurement import FloatInput, IntegerInput
 from windows.table_window import TableWindow
 import pandas as pd
 
 from datetime import datetime
 from time import sleep
+from typing import Dict, List
 
 
 class Test:
@@ -56,6 +58,10 @@ class Emitter(Thread):
 
 class Main(QtWidgets.QMainWindow):
 
+    input_validators = {int: QtGui.QIntValidator,  # Qt-internal checks for different input widget types
+                        float: QtGui.QDoubleValidator,
+                        str: None}
+    
     def __init__(self):
         super(Main, self).__init__()
 
@@ -67,7 +73,16 @@ class Main(QtWidgets.QMainWindow):
         si.finished.connect(self.__finished)
         self.e.start()
 
+        self.__directory_name = ""
+        self.__dynamic_inputs = list()  # type: List[QtWidgets.QLineEdit]
+        
         self.__init_gui()
+
+        # DEBUG:
+        self.__create_input_ui({
+            'v': FloatInput('Maximum Voltage', default=0.0),
+            'i': FloatInput('Current Limit', default=1e-6),
+            'nplc': IntegerInput('NPLC', default=1)})
 
     def __init_gui(self):
         bar = self.menuBar()
@@ -78,9 +93,42 @@ class Main(QtWidgets.QMainWindow):
         for entry in registry:
             method.addAction(entry, lambda x=entry: self.__menu_clicked(x))
 
-        self.__mdi = QtWidgets.QMdiArea()
 
-        self.setCentralWidget(self.__mdi)
+        central_layout = QtWidgets.QHBoxLayout()
+        central_widget = QtWidgets.QWidget()
+        central_widget.setLayout(central_layout)
+        
+        inputs_layout = QtWidgets.QVBoxLayout()
+        central_layout.addLayout(inputs_layout)
+
+        file_name_layout = QtWidgets.QVBoxLayout()
+        inputs_layout.addLayout(file_name_layout)
+        file_name_layout.setSpacing(5)
+        file_name_layout.addWidget(QtWidgets.QLabel("Save directory:"))
+        self.__file_name_display = QtWidgets.QLineEdit()
+        self.__file_name_display.setFixedWidth(180)  # Also sets left panel width
+        file_name_layout.addWidget(self.__file_name_display)
+        self.__file_name_display.setReadOnly(True)
+        self.__file_name_display.textChanged.connect(
+            lambda text: self.__file_name_display.setToolTip(text)
+        )
+        self.__file_name_display.setText(self.__directory_name)
+        file_name_button = QtWidgets.QPushButton()
+        file_name_layout.addWidget(file_name_button)
+        file_name_button.setText("Select directory...")
+        file_name_button.clicked.connect(self.__set_directory_name)
+
+        # This layout contains the dynamically managed inputs:
+        self.__dynamic_inputs_layout = QtWidgets.QVBoxLayout()
+        self.__dynamic_inputs_layout.setSpacing(15)
+        file_name_layout.addLayout(self.__dynamic_inputs_layout)
+
+        inputs_layout.addStretch()
+
+        self.__mdi = QtWidgets.QMdiArea()
+        central_layout.addWidget(self.__mdi)
+
+        self.setCentralWidget(central_widget)
 
         self.__tb_window = TableWindow()
         self.__mdi.addSubWindow(self.__tb_window)
@@ -113,6 +161,41 @@ class Main(QtWidgets.QMainWindow):
             self.__handle_strategies[data_type](data_object)
         else:
             print(data_object)
+
+    def __create_input_ui(self, inputs):
+        """Dynamically set left panel user input widgets.
+        
+        Arguments:
+            inputs: Dict[str, AbstractInput]: A dictionary of inputs as defined in SMU2Probe.inputs
+        """
+        self.__dynamic_inputs.clear()
+
+        for element in list(inputs.keys()):
+            element_layout = QtWidgets.QVBoxLayout()
+            element_layout.setSpacing(0)
+            self.__dynamic_inputs_layout.addLayout(element_layout)
+
+            element_name = inputs[element].fullname
+            element_layout.addWidget(QtWidgets.QLabel(element_name))  # Header text
+
+            element_input_field = QtWidgets.QLineEdit()
+            self.__dynamic_inputs.append(element_input_field)
+            element_layout.addWidget(element_input_field)
+            
+            # Validate the input field if it is numerical:
+            element_type = inputs[element].type
+            element_input_validator = self.input_validators[element_type]  # This is a type object
+            if element_input_validator is not None:
+                element_input_field.setValidator(element_input_validator())
+
+            element_default = inputs[element].default
+            element_input_field.setText(str(element_default))
+
+            
+    @QtCore.pyqtSlot()
+    def __set_directory_name(self):
+        """Open a dialogue to change the output directory."""
+        self.__directory_name = QtWidgets.QFileDialog.getExistingDirectoryUrl().path() + "/"
 
 
 
